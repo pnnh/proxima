41d26e8cfaf3e09c4ecb542fe6bd9f2aaff07e7b
larry <xspanni@gmail.com>
2026-03-13 15:58:43 +0800

    移除Android多余文件，移动到根目录下cmake配置 将iOS项目添加到xcworkspace统一管理

--- euler/Venus/ContentView.swift
+++ euler/Venus/ContentView.swift
@@ -55,7 +55,7 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
-}
+//#Preview {
+//    ContentView()
+//        .modelContainer(for: Item.self, inMemory: true)
+//}
